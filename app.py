8222a0b6960162dbdfd89792d9b26f075645a4c5
Jake Poirier <jake.poirier@stash.com>
2022-01-17 12:06:22 -0500

    Gettin lazy, added a dataset to pick around with instead

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 from flask import Flask, render_template, request
 import http_client
@@ -12,24 +13,36 @@ def hello_world():
 @app.route('/book')
 def book():
     author = request.args.get("author")
+    if author is None:
+        return "No author parameter"
     client = http_client.HttpClient()
     base_url = "https://reststop.randomhouse.com/resources/works"
-    start = 0
-    max = 0
-    expandLevel = 0
-    url = f'{base_url}?start={start}&max={max}&expandLevel={expandLevel}&search={author}'
+    url = f'{base_url}?start=0&max=0&expandLevel=0&search={author}'
+    print(url)
     data = client.get(url)
     books = []
-    for uri in json.loads(data)['work']:
-        d = json.loads(client.get(uri['@uri']))
-        print(d)
-        code = d['titles']['isbn'][0]['$']
-        t = f"https://covers.openlibrary.org/b/isbn/{code}-M.jpg"
-        books.append(t)
-        print(books)
+    book_obj = json.loads(data)['work']
+    print(book_obj)
+    random_int = random.randint(0, len(book_obj)-1)
+    uri = book_obj[random_int]
+    img_obj = client.get(uri['@uri'])
+    print(img_obj)
+    if img_obj:
+        titles = json.loads(img_obj)['titles']['isbn']
+        if type(titles) == list:
+            isbn = titles[0]['$']
+        else:
+            isbn = titles['$']
+        print(isbn)
+    # for uri in json.loads(data)['work']:
+    #     d = json.loads(client.get(uri['@uri']))
+    #     print(d)
+    #     code = d['titles']['isbn'][0]['$']
+    #     t = f"https://covers.openlibrary.org/b/isbn/{code}-M.jpg"
+    #     books.append(t)
     # print(books)
     return render_template('books.html', data=books, default_image_url='/static/images/default-placeholder.png')
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5556, debug=True)
+    app.run(host='0.0.0.0', port=5556, debug=True)
--- http_client.py
+++ http_client.py
@@ -1,22 +1,17 @@
 import logging
 import requests
-# import backoff
-import urllib.parse
 
 logger = logging.getLogger()
 logger.setLevel("INFO")
 
 
 class HttpClient:
-
-    # @backoff.on_exception(backoff.expo, requests.exceptions.RequestException, max_time=60, logger=logger)
     def get(self, url):
         headers = {"Accept": "application/json"}
-        # encoded_url = urllib.parse.quote(url)
-        # print(encoded_url)
         data = requests.get(url, headers=headers)
         if data.status_code != 200:
             logger.error('Error')
             return ''
         else:
             return data.text
+
